557a8f4c530f319acb657e581d47cc4c1373db2a
julianfrancodev <juliandanielfranco@gmail.com>
2025-01-04 10:31:19 -0500

    feat: add new card to project listing

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
-import {SafeAreaView } from 'react-native'
+import { SafeAreaView } from 'react-native'
 import { LoginHeader } from '../organisms/loginItemsListOrganism/LoginHeader'
 import { LoginInputItems } from '../organisms/loginItemsListOrganism/LoginInputItems'
+import { ScrollView } from 'react-native-virtualized-view'
 
 export const LoginPage = () => {
     return (
-        <SafeAreaView style={{flex: 1}}>
-           <LoginHeader/>
-           <LoginInputItems/>
+        <SafeAreaView style={{ flex: 1 }}>
+            <ScrollView>
+                <LoginHeader />
+                <LoginInputItems />
+            </ScrollView>
         </SafeAreaView>
     )
 }
